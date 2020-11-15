268116efeaa0fca40de5df2b3a099e0b96d7b181
Miquelguix <guixdades@gmail.com>
2020-11-15 22:15:01 +0100

    estructura bàsica i seeder de gobierno

---
+++ database/migrations/2020_11_15_190002_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('link', 128);
+            $table->string('qualification', 16);
+            $table->string('price',16);
+            $table->string('excerpt', 16);
+            $table->longText('body1')->nullable();
+            $table->longText('body2')->nullable();
+            $table->longText('body3')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
